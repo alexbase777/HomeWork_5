f1e07c89e174f2cc22221e6d4cbb4e8692232f15
Алексей <tm23515417@gmail.com>
2024-07-06 08:36:14 +0500

    Исправленная версия

--- HomeWork_4/End_Window.xaml.cs
+++ HomeWork_4/End_Window.xaml.cs
@@ -12,48 +12,81 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace HomeWork_4
 {
     /// <summary>
     /// Interaction logic for End_Window.xaml
     /// </summary>
-    public partial class End_Window : Window
+    public partial class End_Window : Window, INotifyPropertyChanged
     {
+        public List<ToDo> TodoList;
+        public List<ToDo> todoList
+        {
+            get { return TodoList; }
+            set
+            {
+                TodoList = value;
+                OnPropertyChanged();
+            }
+        }
+        public int CountDoing { get; set; }
         public End_Window()
         {            
-            InitializeComponent();            
+            InitializeComponent();
+            DataContext = this;
+            TodoList = new List<ToDo>();
+            TodoList = MainWindow.toDoList;
+
             DataToDoList.ItemsSource = MainWindow.toDoList;
-            EndToDo();
+            //EndToDo();
+            OnPropertyChanged();
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged()
+        {
+            CountDoing = TodoList.Where(e => e.Doing == true).ToList().Count;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TodoList"));   
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CountDoing"));
+        }
+
         private void ButtonDeleteToDo(object sender, RoutedEventArgs e)
         {
             MainWindow.toDoList.Remove((ToDo)DataToDoList.SelectedItem);
             DataToDoList.ItemsSource = null;
             DataToDoList.ItemsSource = MainWindow.toDoList;
-            EndToDo();
+            //EndToDo();
+
+            OnPropertyChanged();
         }
         private void ButtonAddToDo(object sender, RoutedEventArgs e)
         {
             Second_Window second_Window = new Second_Window();
             second_Window.Show();
             second_Window.Owner = this;
-            EndToDo();
+            //EndToDo();
+            OnPropertyChanged();
         }
         private void CheckboxEnableToDo_Checked(object sender, RoutedEventArgs e)
         {
             if (DataToDoList.SelectedItem == null || AddToDo == null) return;
             MainWindow.toDoList[DataToDoList.SelectedIndex].Doing = true;
-            EndToDo();
+            //EndToDo();
+            OnPropertyChanged();
         }
         private void CheckboxEnableToDo_Unchecked(object sender, RoutedEventArgs e)
         {
             if (DataToDoList.SelectedItem == null || AddToDo == null) return;
             MainWindow.toDoList[DataToDoList.SelectedIndex].Doing = false;
-            EndToDo();
-        }
+            //EndToDo();
+            OnPropertyChanged();
+        }      
 
-        private void EndToDo()
+        /*private void EndToDo() // Неэффективный метод по заданию
         {   
             ProgressBarEnd.Value = 0;
             ProgressBarEnd.Minimum = 0;
@@ -62,6 +95,6 @@ namespace HomeWork_4
                 if (item.Doing)
                     ProgressBarEnd.Value++;
             TextBlockProgress.Text = ProgressBarEnd.Value.ToString() + '/' + MainWindow.toDoList.Count;
-        }      
+        }*/      
     }
 }
